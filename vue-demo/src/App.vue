7a47584ae4014de352e84158e7ef40c02f686044
Meteor owner <107754977+Meteor-owner@users.noreply.github.com>
2024-01-15 20:59:16 +0800

    1.15last

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view v-if="isRouterAlive"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  components: { },
-  data () {
+  components: {},
+  data() {
     return {
-      
+      isRouterAlive: true
+    }
+  },
+  provide() { // 父组件中返回要传给下级的数据
+    return {
+      reload: this.reload
+    }
+  },
+  methods: {
+    reload() {
+      console.log('触发reload函数')
+      this.isRouterAlive = false;
+      this.$nextTick(function () {
+        this.isRouterAlive = true;
+      });
     }
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- vue-demo/src/views/UserView.vue
+++ vue-demo/src/views/UserView.vue
@@ -2,43 +2,39 @@
     <div class="container">
         <main-bar></main-bar>
         <div class="main">
-            <div class="box1">
-                <div class="head-img">
-                    <img src="../assets/img/bnt.gif">
-                </div>
-                <div class="user-description">
-                    <div class="">
-
-                    </div>
-                </div>
-            </div>
             <div class="box2">
                 <el-container>
                     <el-aside width="230px" style="border: 1px solid #eee">
-                        <el-menu :default-openeds="['1', '2']" text-color="#9e7d81" active-text-color="#fff"
-                            default-active="1-1" @select="selectComponent">
-                            <el-submenu index="1">
-                                <template slot="title"><i class="el-icon-message"></i>个人中心</template>
-                                <el-menu-item index="1-1">我的关注</el-menu-item>
-                                <el-menu-item index="1-2">我的发布</el-menu-item>
-                                <el-menu-item index="1-3">浏览记录</el-menu-item>
-                            </el-submenu>
-                            <el-submenu index="2">
-                                <template slot="title"><i class="el-icon-setting"></i>账户设置</template>
-                                <el-menu-item index="2-1">个人资料</el-menu-item>
-                                <el-menu-item index="2-2">密码设置</el-menu-item>
-                            </el-submenu>
-                        </el-menu>
+                        <div style="background: #fff; border-radius: 10px; height: 700px;">
+                            <div class="head-img">
+                                <el-avatar :size="120" fit="scale-down" @error="errorHandler">
+                                    <el-image :src="require('../assets/img/head-img.jpg')"></el-image>
+                                </el-avatar>
+                            </div>
+                            <div class="username">
+                                <span>{{ name }}</span>
+                            </div>
+                            <el-menu :default-openeds="['1', '2']" text-color="#9e7d81" active-text-color="#fff"
+                                default-active="1-1">
+                                <el-submenu index="1">
+                                    <template slot="title"><i class="el-icon-message"></i>个人中心</template>
+                                    <el-menu-item index="1-1" @click="selectComponent('1-1')">我的关注</el-menu-item>
+                                    <el-menu-item index="1-2" @click="selectComponent('1-2')">我的发布</el-menu-item>
+                                    <el-menu-item index="1-3" @click="selectComponent('1-3')">我的租房</el-menu-item>
+                                    <el-menu-item index="1-4" @click="selectComponent('1-4')">浏览记录</el-menu-item>
+                                </el-submenu>
+                                <el-submenu index="2">
+                                    <template slot="title"><i class="el-icon-setting"></i>账户设置</template>
+                                    <el-menu-item index="2-1" @click="selectComponent('2-1')">个人资料</el-menu-item>
+                                </el-submenu>
+                            </el-menu>
+                        </div>
+
                     </el-aside>
                     <el-main>
-                        <el-table :data="tableData" border v-if="curIndex === '2-1'">
-                            <el-table-column prop="name" label="名称" width="250"></el-table-column>
-                            <el-table-column prop="updatetime" label="最后操作时间" width="250"></el-table-column>
-                            <el-table-column label="操作">
-                                <el-button type="primary" size="mini">编辑</el-button>
-                                <el-button type="danger" size="mini">删除</el-button>
-                            </el-table-column>
-                        </el-table>
+                        <div class="content">
+                            <component :is="curComponent"></component>
+                        </div>
                     </el-main>
                 </el-container>
             </div>
@@ -50,21 +46,43 @@
 <script>
 import FooterBar from '../components/FooterBar.vue'
 import MainBar from "../components/MainBar.vue"
+import InfoView from './InfoView.vue'
 
 export default {
-    components: { MainBar, FooterBar },
+    components: { MainBar, FooterBar, InfoView },
+    inject: ['reload'],
     data() {
         return {
-            curComponent: null,
-            curIndex: 2-1,
+            curComponent: 'FollowView',
+            curIndex: 1 - 1,
+            name: "Meteor",
+            signal: "light",
+            // tableData: [{
+            //     id: 1,
+            //     name: "meteor",
+            //     signal: "light"
+            // }]
         }
     },
     methods: {
         selectComponent(index) {
-            if (index == -'1-1') {
-                this.curComponent = 'FooterBar';
-                console.log(this.curComponent);
+            console.log(index);
+            if (index === '1-1') {
+                this.curComponent = 'FollowView';
+            } else if (index === '1-2') {
+                this.curComponent = 'PublishView';
+            } else if (index === '1-3') {
+                this.curComponent = 'RentalView';
+            } else if (index === '1-4') {
+                this.curComponent = 'BrowseView';
+            } else if (index === '2-1') {
+                this.curComponent = 'InfoView';
             }
+            console.log(this.curComponent);
+
+        },
+        errorHandler() {
+            return true
         }
     }
 }
@@ -77,26 +95,18 @@ export default {
 
 .main {
     padding: 0 10%;
-    margin: 3px 10%;
+    margin: 0.7% 10%;
     width: 60%;
-    border: #9e9e9e 1px solid;
+    /* border: #9e9e9e 1px solid; */
     height: auto;
     align-items: center;
-}
-
-.box1 {
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
-    border: red 1px solid;
-    height: 200px;
-    padding: 10px;
 }
 
 .head-img {
     width: 120px;
     height: 120px;
-    margin-top: 3%;
+    margin: 3% 0 7% 20%;
+    padding-top: 10%;
 }
 
 .head-img img {
@@ -104,26 +114,41 @@ export default {
     width: auto;
 }
 
-.user-description {
-    width: 70%;
-    margin-left: -10%;
-    border: #9e9e9e 1px solid;
+.username {
+    margin: auto;
+    margin-bottom: 7%;
+}
+
+.username span {
+    font-size: 20px;
+    color: #9e7d81;
 }
 
 .box2 {
     margin: 0 auto;
-    height: 800px;
-    border: red 1px solid;
+    /* height: 800px; */
+    /* border: red 1px solid; */
     display: flex;
     justify-content: space-between;
+    text-align: center
+}
+
+.content {
+    width: 100%;
+    height: 100%;
+    padding: 0 5%;
 }
 
 .el-aside {
     padding: 10px;
+    border: 0 !important;
+    user-select: none;
 }
 
 .el-main {
     padding: 10px;
+    background: #fff;
+    border-radius: 10px;
 }
 
 .el-menu-item.is-active {
